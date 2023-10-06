dee7806abe0eeedd196ef51d81f26f5e89825058
Kemel Zaidan <kemelzaidan@gmail.com>
2023-10-06 04:29:02 -0300

    fixes description for non-file tools

--- conf.d/funcoeszz.fish
+++ conf.d/funcoeszz.fish
@@ -1,8 +1,14 @@
 if type -q sed
-  for file in {(dirname $ZZPATH)/zz/*,zzajuda,zztool,zzzz}
+  for file in (dirname $ZZPATH)/zz/*
     set func (basename $file .sh)
     set desc (sed '2!d' $file | cut -b 3-)
     function $func -V func -d "$desc"
+      $ZZPATH $func $argv
+    end
+  end
+  for tool in {zzajuda,zztool,zzzz}
+    set func $tool
+    function $func -V func -d "funcoeszz"
       $ZZPATH $func $argv
     end
   end
